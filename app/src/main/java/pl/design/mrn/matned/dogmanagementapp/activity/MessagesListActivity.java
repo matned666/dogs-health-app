5c8fcf6caa2b621a366febf16130c566db9e509f
Mateusz Niedbal <mat.niedbal@yahoo.com>
2020-08-06 12:04:57 +0200

    messages layout without ids messages element layout without ids Emergency activity changed to messages activity

--- app/src/main/java/pl/design/mrn/matned/dogmanagementapp/activity/MessagesListActivity.java
+++ app/src/main/java/pl/design/mrn/matned/dogmanagementapp/activity/MessagesListActivity.java
@@ -6,13 +6,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import pl.design.mrn.matned.dogmanagementapp.R;
 
-public class EmergencyActivity extends AppCompatActivity {
+public class MessagesListActivity extends AppCompatActivity {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.emergency_activity);
+        setContentView(R.layout.messages_activity);
     }
 
 
